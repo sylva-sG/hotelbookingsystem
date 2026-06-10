37571f04a14003c1ad4fd5f6fe84f6dd2433e480
sylva-sG <sylvansaoko@gmail.com>
2026-06-10 15:09:40 +0300

    create guest class and room class

---
+++ models/guest.py
@@ -0,0 +1,32 @@
+from person import Person
+
+class Guest(Person):
+
+    guest_count = 0
+
+    def __init__(self, name, email):
+        super().__init__(name, age=None)
+        self.email = email
+        Guest.guest_count += 1
+        self.id = Guest.guest_count
+        self.email = email
+        self.reservations = []
+
+    @property
+    def email(self):
+        return self._email
+    
+    @email.setter
+    def email(self, value):
+        if "@" not in value:
+            raise ValueError("Invalid email address")
+        self._email = value
+
+    def add_reservation(self, reservation):
+        self.reservations.append(reservation)
+        
+    def __str__(self):
+        return f"Guest ID: {self.id} | {self.name} | {self.email}"
+    
+guest = Guest("sylvans", "sylvans@example.com")
+print(guest)
